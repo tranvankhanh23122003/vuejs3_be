f3a3cc7f5acb18c2f7a652af93a25427800acb48
TRANVANKHANH <tranvankhanh23122003@gmail>
2025-03-11 01:31:06 +0700

    thêm required vs unique trong usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,37 +17,55 @@ class UserController extends Controller
     }
     public function index()
     {
-        $users = User::
-        join('departments', 'users.department_id', '=', 'departments.id')
-        ->join('users_status', 'users.status', '=', 'users_status.id')
-        ->select('users.*',
-        'departments.name as departments',
-         'users_status.name as status')
-        ->get();
+        $users = User::join('departments', 'users.department_id', '=', 'departments.id')
+            ->join('users_status', 'users.status', '=', 'users_status.id')
+            ->select(
+                'users.*',
+                'departments.name as departments',
+                'users_status.name as status'
+            )
+            ->get();
         return response()->json($users);
-
     }
-    public function create(){
-$users_status = DB::table('users_status')
-->select(
-    "id as value",
-    "name as label",
-)
-->get();
-$departments = DB::table('departments')
-->select(
-    "id as value",
-    "name as label",
-)
-->get();
-return response()->json([
-"users_status" => $users_status,
-"departments"=>$departments,
-]);
+    public function create()
+    {
+        $users_status = DB::table('users_status')
+            ->select(
+                "id as value",
+                "name as label",
+            )
+            ->get();
+        $departments = DB::table('departments')
+            ->select(
+                "id as value",
+                "name as label",
+            )
+            ->get();
+        return response()->json([
+            "users_status" => $users_status,
+            "departments" => $departments,
+        ]);
     }
     public function store(Request $request)
     {
-return $request;
+        $validated = $request->validate([
+"status" => "required",
+"username" => "required|unique:users,username",
+"name" => "required|max:255",
+"email" => "required|email",
+"department_id" => "required",
+"password" => "required|confirmed"
 
+        ],[
+        "status.required" => " Vui lòn Nhập tình trạng ",
+        "usename.required" => " Vui lòn Nhập Tên tài khoản ",
+        "usename.unique" => " Tên tài khoản đã tồn tại ",
+        "name.required" => " Vui lòn Nhập Họ và tên ",
+        "email.required" => " Vui lòn Nhập email ",
+        "department_id.required" => " Vui lòn Nhập phòng ban ",
+        "password.required" => "  Vui lòn Nhập mật khẩu ",
+        "password.required" =>" Vui lòng nhập xác nhận mật khẩu"
+
+    ]);
     }
 }
