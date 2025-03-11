2853473de459b68da643363e6d7a7fac3e5570a5
TRANVANKHANH <tranvankhanh23122003@gmail>
2025-03-12 02:16:14 +0700

    sử dụng Inserts dữ liệu với Eloquent ORM và Query Builder để Tạo mới Tài khoản

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 
 
 class UserController extends Controller
@@ -48,24 +50,36 @@ class UserController extends Controller
     }
     public function store(Request $request)
     {
-        $validated = $request->validate([
-"status" => "required",
-"username" => "required|unique:users,username",
-"name" => "required|max:255",
-"email" => "required|email",
-"department_id" => "required",
-"password" => "required|confirmed"
+        //         $validated = $request->validate([
+        // "status" => "required",
+        // "username" => "required|unique:users,username",
+        // "name" => "required|max:255",
+        // "email" => "required|email",
+        // "department_id" => "required",
+        // "password" => "required|confirmed"
 
-        ],[
-        "status.required" => " Vui lòn Nhập tình trạng ",
-        "usename.required" => " Vui lòn Nhập Tên tài khoản ",
-        "usename.unique" => " Tên tài khoản đã tồn tại ",
-        "name.required" => " Vui lòn Nhập Họ và tên ",
-        "email.required" => " Vui lòn Nhập email ",
-        "department_id.required" => " Vui lòn Nhập phòng ban ",
-        "password.required" => "  Vui lòn Nhập mật khẩu ",
-        "password.required" =>" Vui lòng nhập xác nhận mật khẩu"
+        //         ],[
+        //         "status.required" => " Vui lòn Nhập tình trạng ",
+        //         "usename.required" => " Vui lòn Nhập Tên tài khoản ",
+        //         "usename.unique" => " Tên tài khoản đã tồn tại ",
+        //         "name.required" => " Vui lòn Nhập Họ và tên ",
+        //         "email.required" => " Vui lòn Nhập email ",
+        //         "department_id.required" => " Vui lòn Nhập phòng ban ",
+        //         "password.required" => "  Vui lòn Nhập mật khẩu ",
+        //         "password.required" =>" Vui lòng nhập xác nhận mật khẩu"
 
-    ]);
+        //     ]);
+        // return $request;
+
+        //Eloquent ORM
+        User::create([
+            "status" =>$request["status"],
+            "username" =>$request["username"],
+            "name" =>$request["name"],
+            "email" =>$request["email"],
+            "department_id" =>$request["department_id"],
+            "password" =>Hash::make($request["password"]),
+        ]);
+
     }
 }
