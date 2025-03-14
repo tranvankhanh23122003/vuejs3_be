38e03812dbf3c09db9abee905577bae17ee19022
TRANVANKHANH <tranvankhanh23122003@gmail>
2025-03-15 02:11:42 +0700

    thêm api xóa tài khoản và làm ẩn id 1 của admin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Psy\Readline\Hoa\Console;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
@@ -18,7 +19,9 @@ class UserController extends Controller
     }
     public function index()
     {
-        $users = User::join('departments', 'users.department_id', '=', 'departments.id')
+        $users = User::
+        // where("users.id" , "!" ,"1")
+        join('departments', 'users.department_id', '=', 'departments.id')
             ->join('users_status', 'users.status', '=', 'users_status.id')
             ->select(
                 'users.*',
@@ -111,5 +114,62 @@ class UserController extends Controller
             "users_status" => $users_status,
             "departments" => $departments,
         ]);
+    }
+    function update(Request $request, $id)
+    {
+        // $validated = $request->validate([
+        //     "status" => "required",
+        //     "username" => "required|unique:users,username,".$id,
+        //     "name" => "required|max:255",
+        //     "email" => "required|email",
+        //     "department_id" => "required",
+
+        // ], [
+        //     "status.required" => " Vui lòn Nhập tình trạng ",
+        //     "username.required" => " Vui lòn Nhập Tên tài khoản ",
+        //     "username.unique" => " Tên tài khoản đã tồn tại ",
+        //     "name.required" => " Vui lòn Nhập Họ và tên ",
+        //     "email.required" => " Vui lòn Nhập email ",
+        //     "department_id.required" => " Vui lòn Nhập phòng ban ",
+
+        // ]);
+        // return "ok";
+        if ($request["change_password"] == false) {
+            $update = User::find($id)->update([
+                "status" => $request["status"],
+                "username" => $request["username"],
+                "name" => $request["name"],
+                "email" => $request["email"],
+                "department_id" => $request["department_id"],
+            ]);
+
+            if ($update) {
+                return response()->json([
+                    'message' => 'Cập nhật thành công'
+                ], Response::HTTP_OK);
+            }
+        } else {
+            $validated = $request->validate([
+                'password' => 'required|confirmed|min:6',  // Password cần phải được xác nhận và ít nhất 6 ký tự
+            ], [
+                "password.required" => "Nhập mật khẩu ",
+                "password.min" => "Mật khẩu ít nhất 6 ký tự ",
+                "password.confirmed" => "Mật khẩu xác nhận không khớp", // Thêm thông báo lỗi khi không khớp
+            ]);
+
+            $update = User::find($id)->update([
+                "password" => Hash::make($request["password"]),
+                "change_password_at" => NOW(),
+            ]);
+
+            if ($update) {
+                return response()->json([
+                    'message' => 'Đổi pass thành công'
+                ], Response::HTTP_OK);
+            }
+        }
+    }
+    function destroy($id){
+       User::find($id)->delete();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,10 @@ Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/create', [UserController::class, 'create']);
 Route::post('/users', [UserController::class, 'store']);
 Route::get('/users/{id}/edit', [UserController::class, 'edit']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+
 
 
 Route::get('test', function (Request $request) {
